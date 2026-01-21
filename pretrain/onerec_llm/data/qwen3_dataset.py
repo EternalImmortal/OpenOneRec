a6266bfdbed60204a0a38747494cc841b2fc8bd0
josephwei <weijosephrj@gmail.com>
2026-01-21 16:07:39 +0800

    默认parquet_backend参数改为pyarrow

--- pretrain/onerec_llm/data/qwen3_dataset.py
+++ pretrain/onerec_llm/data/qwen3_dataset.py
@@ -482,14 +482,14 @@ class Qwen3ChatCompletionDataset(IterableDataset):
 
                 self.source_sample_cnt.setdefault(source_name, 0)
                 self.source_sample_cnt[source_name] += 1
-            
+
                 inputs = self._process(sample, source_name)
             except:
                 self.source_error_cnt.setdefault(source_name, 0)
                 self.source_error_cnt[source_name] += 1
                 error_ratio = self.source_error_cnt[source_name] * 1.0 / \
                     self.source_sample_cnt[source_name]
-                
+
                 rank, world_size, worker, num_workers = pytorch_worker_info()
                 logger.error(
                     f"Qwen3ChatCompletionDataset process sample error. worker=r{rank}_w{worker}"
