24c069e1273a301b1e36fa2b3405d38c95b84b74
josephwei <weijosephrj@gmail.com>
2026-01-26 12:32:50 +0800

    udpate testv2

--- pretrain/scripts/test_hf_model_2.sh
+++ pretrain/scripts/test_hf_model_2.sh
@@ -35,7 +35,7 @@ USE_DEFAULT=true
 
 # Option 2: Use custom test file (comment out USE_DEFAULT and uncomment below)
  USE_DEFAULT=false
- TEST_FILE=tools/model_test/test_pretrain_v1.json
+ TEST_FILE=tools/model_test/test_pretrain_v2.json
 
 # Generation parameters
 MAX_NEW_TOKENS=4096
